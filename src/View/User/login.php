6f8b7ec09fe598d5b6316f2161d5646452816ee1
emmanuel.ratier@epitech.eu <emmanuel.ratier@epitech.eu@epitech.eu>
2018-09-19 14:48:09 +0200

    first commit

---
+++ src/View/User/login.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+    <form action="/index.php" method="post">
+       <label for="email">MAIL</label>
+        <input type="email" id="email" name="email">
+
+        <label for="password">Password</label>
+        <input type="password" name="password" id="psw">
+
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
